13e21d08f5024323894ea6d99cf879e82b2e7ebf
Bharatrathod-81 <94593235+Bharatrathod-81@users.noreply.github.com>
2022-05-21 02:50:56 +0530

    feat: Added Rules page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { Routes , Route} from "react-router-dom"
 import Home from "../pages/home-page/home-page";
+import RulePage from "../pages/rules-page/rule-page";
 
 const AllRoutes = () => {
 
     return(
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/rules/:gameNameId' element={<RulePage />} />
         </Routes>
     )
 }
--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -1,5 +1,6 @@
 import "./home-page.css";
 import { useDataContext } from "../../contexts/data-context";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 
@@ -11,12 +12,12 @@ const Home = () => {
             <div className="card-container jstfy-centre align-centre margin-Xsmall">
                 {dataBasearr.map(item => {
                     return (
-                        <div key={item.gameName} className="card margin-Xsmall">
-                            <img className="program-img" src={item.image} alt="react img" />
+                        <Link to={`/rules/${item.gameName}`} key={item.gameName} className="card margin-Xsmall" >
+                            <img className="program-img margin-small" src={item.image} alt="react img" />
                             <p className="card-text">Quiz on {item.gameName}</p>
                             <small className="card-text">Take this quiz to test yourself!</small>
                             <small className="card-text">5 Question</small>
-                        </div>
+                        </Link>
                     )
                 })}
             </div>
