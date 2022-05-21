ca8f3b23a1e6b3aa40ac09b470a40cbea405f211
Bharatrathod-81 <94593235+Bharatrathod-81@users.noreply.github.com>
2022-05-21 16:58:13 +0530

    feat: Added result-page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { Routes , Route} from "react-router-dom"
 import Home from "../pages/home-page/home-page";
 import RulePage from "../pages/rules-page/rule-page";
 import PlayPage from "../pages/play-page/play-page";
+import ResultPage from "../pages/result-page/result-page";
 
 const AllRoutes = () => {
 
@@ -10,6 +11,7 @@ const AllRoutes = () => {
             <Route path='/' element={<Home />} />
             <Route path='/rules/:gameName' element={<RulePage />} />
             <Route path='/play-game/:gameName' element={<PlayPage />} />
+            <Route path='/result/:gameName' element={<ResultPage />} />
         </Routes>
     )
 }
