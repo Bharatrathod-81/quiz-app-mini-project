a181ed6d73850a07b5de1b695ed476790aa6840f
Bharatrathod-81 <94593235+Bharatrathod-81@users.noreply.github.com>
2022-08-13 12:28:59 +0530

    fix:bug

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
 
-    const { dataBasearr } = useDataContext();
+    const { dataBaseArray } = useDataContext();
 
     return (
         <div className="home-container jstfy-centre align-centre flex-column jstfy-spce-around">
             <h1 className="home-heading">Quiz on Programming Language</h1>
             <div className="card-container jstfy-centre align-centre margin-Xsmall">
-                {dataBasearr.map(item => {
+                {dataBaseArray.map(item => {
                     return (
                         <Link to={`/rules/${item.gameName}`} key={item.gameName} className="card margin-Xsmall" >
                             <img className="program-img margin-small" src={item.image} alt="react img" />
